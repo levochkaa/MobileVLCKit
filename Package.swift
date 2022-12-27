dece8622e196011ba5dc9832d9d2570bc4725f2f
Lev Poznyakov <levochkaaa@icloud.com>
2022-12-28 01:33:56 +0300

    updated vlckit to 3.5.1 and added local .xcframework

--- Package.swift
+++ Package.swift
@@ -21,11 +21,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(
-            name: "MobileVLCKit",
-            url: "https://instaply-public-libs.s3.eu-west-1.amazonaws.com/ios/MobileVLCKit.xcframework.zip",
-            checksum: "4a2bf225beef946a75b33e1854c300382091e5c8e28d1bcff47c2ce74a90a67d"
-        )
+        .binaryTarget(name: "MobileVLCKit", path: "MobileVLCKit.xcframework")
 //        .target(
 //            name: "MobileVLCKit",
 //            dependencies: []),
