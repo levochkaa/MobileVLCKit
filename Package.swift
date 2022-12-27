e6e9ae717204e4e7b8831b8098af7fc87b4133b5
Lev Poznyakov <levochkaaa@icloud.com>
2022-12-27 21:57:28 +0300

    set up

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MobileVLCKit",
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -18,11 +21,16 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .binaryTarget(
             name: "MobileVLCKit",
-            dependencies: []),
-        .testTarget(
-            name: "MobileVLCKitTests",
-            dependencies: ["MobileVLCKit"]),
+            url: "https://instaply-public-libs.s3.eu-west-1.amazonaws.com/ios/MobileVLCKit.xcframework.zip",
+            checksum: "4a2bf225beef946a75b33e1854c300382091e5c8e28d1bcff47c2ce74a90a67d"
+        )
+//        .target(
+//            name: "MobileVLCKit",
+//            dependencies: []),
+//        .testTarget(
+//            name: "MobileVLCKitTests",
+//            dependencies: ["MobileVLCKit"]),
     ]
 )
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # MobileVLCKit
 
-A description of this package.
+A repository for Telegram client BetterTG to use VLCKit through Swift Package Manager (not Pods).\
+Waiting for 4.0, where VideoLan will support SPM.\
+Stay updated: https://code.videolan.org/videolan/VLCKit/-/issues/302
